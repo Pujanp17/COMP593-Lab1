07890d81d0ca4901b638729387222b598156547d
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-05-28 02:36:13 +0530

    Modified both scripts.
    Removed one print statement from scritp1.py
    Added one more print statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #This code is taken from Zen of python. 
 print("There should be one-- and preferably only one --obvious way to do it.")
-print("Although that way may not be obvious at first unless you're Dutch.")
--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@
 print("Simple is better than complex.")
 print("Complex is better than complicated.")
 print("Flat is better than nested.")
+print("Although that way may not be obvious at first unless you're Dutch.")
